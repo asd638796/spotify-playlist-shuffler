284599a8a1057ebf24f9ef4c8c6d5184657c5917
Sheriell Dar <asd638796@gmail.com>
2024-07-01 22:33:17 +0100

    refactor backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,47 @@
 import React, { useEffect, useState } from 'react';
-import { redirectToAuthCodeFlow, getAccessToken } from './script';
-
+import { redirectToAuthCodeFlow, getAccessToken, getItemsFromPlaylist, shuffleArray, addToQueue, refreshAccessToken } from './script';
 
 function App() {
-  
   const [token, setToken] = useState('');
   const [playlistId, setPlaylistId] = useState('');
-  const [numTracks, setNumTracks] = useState(10); // State for number of tracks
-  const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+  const [numTracks, setNumTracks] = useState(10);
 
   useEffect(() => {
-    async function authenticate() {
+      
+      
+
+      async function authenticate() {
       const params = new URLSearchParams(window.location.search);
       const code = params.get('code');
 
       if (!code) {
-        redirectToAuthCodeFlow(clientId);
+        redirectToAuthCodeFlow();
+        
       } else {
-        const accessToken = await getAccessToken(clientId, code);
+        
+
+        const {accessToken, refreshToken} = await getAccessToken(code);
+        
         setToken(accessToken);
+        localStorage.setItem('refreshToken', refreshToken);
         localStorage.setItem('accessToken', accessToken);
+        
       }
     }
 
+  
+
     const storedToken = localStorage.getItem('accessToken');
-    if (storedToken) {
+    
+    if (storedToken && storedToken !== 'undefined') {
+      
       setToken(storedToken);
     } else {
       authenticate();
     }
-  }, [clientId]);
-
-  async function fetchWebApi(endpoint, method = 'GET', body) {
-    console.log(`Fetching: https://api.spotify.com/${endpoint}`); // Log the request URL
-    const res = await fetch(`https://api.spotify.com/${endpoint}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json'
-      },
-      method,
-      body: body ? JSON.stringify(body) : null
-    });
-  
-    if (!res.ok) {
-      console.error('Error fetching data:', res.status, res.statusText);
-      return null;
-    }
-  
-    try {
-      const text = await res.text();
-      return text ? JSON.parse(text) : {}; // Handle empty responses
-    } catch (error) {
-      console.error('Error parsing JSON:', error);
-      return null;
-    }
-  }
-  
-
-  async function getItemsFromPlaylist(playlistId) {
-    console.log(`Getting items from playlist: ${playlistId}`); // Log the playlist ID
-
-    let allItems = [];
-    let offset = 0;
-    let limit = 100;
-    let hasMoreTracks = true;
-
-    while (hasMoreTracks) {
-      const data = await fetchWebApi(`v1/playlists/${playlistId}/tracks?offset=${offset}&limit=${limit}`);
-      if (!data) break;
-      allItems = allItems.concat(data.items);
-      offset += limit;
-      hasMoreTracks = data.items.length === limit;
-    }
-
-    
-    return allItems;
-  }
 
 
-
-  function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  }
+  }, []);
 
   function extractPlaylistId(url) {
     const regex = /playlist\/([a-zA-Z0-9]+)(?:\?|$|\/|\&)/;
@@ -102,86 +58,66 @@ function App() {
       return;
     }
 
-    const items = await getItemsFromPlaylist(extractedPlaylistId);
+    const items = await getItemsFromPlaylist(extractedPlaylistId, token);
     if (!items || items.length === 0) {
       alert('Failed to fetch playlist items or playlist is empty.');
       return;
     }
 
     const shuffledItems = shuffleArray(items.map(item => item.track.uri));
-    const limitShuffledItems = shuffledItems.slice(0, numTracks); // Limit to selected number of tracks
-  
+    const limitShuffledItems = shuffledItems.slice(0, numTracks);
 
     for (const trackUri of limitShuffledItems) {
-      await addToQueue(trackUri);
+      await addToQueue(trackUri, token);
     }
-  }
-
-  async function addToQueue(trackUri) {
-    const endpoint = `v1/me/player/queue?uri=${trackUri}`;
-    const result = await fetchWebApi(endpoint, 'POST');
-    if (result) {
-      console.log(`Track added to queue: ${trackUri}`);
-    } else {
-      console.error('Failed to add track to queue');
-    }
-  }
-
-  function clearLocalStorage() {
-    localStorage.clear();
-    setToken('');
-    console.log('Local storage cleared and token reset');
   }
 
   return (
-    <>
-      <div className='app-body'>
-        <form className='app-form'
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleRandomize();
-          }}
-        >
-          <input 
-            type="text"
-            placeholder="Enter playlist URL"
-            value={playlistId}
-            onChange={(e) => setPlaylistId(e.target.value)}
-          />
-          <div className="form-buttons">
-            <label>
-              <input
-                type="radio"
-                value={5}
-                checked={numTracks === 5}
-                onChange={() => setNumTracks(5)}
-              />
-              5
-            </label>
-            <label>
-              <input
-                type="radio"
-                value={10}
-                checked={numTracks === 10}
-                onChange={() => setNumTracks(10)}
-              />
-              10
-            </label>
-            <label>
-              <input
-                type="radio"
-                value={15}
-                checked={numTracks === 15}
-                onChange={() => setNumTracks(15)}
-              />
-              15
-            </label>
-          </div>
-          <button type="submit" className='form-button'>Randomize</button>
-          
-        </form>
-      </div>
-    </>
+    <div className='app-body'>
+      <form className='app-form'
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleRandomize();
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Enter playlist URL"
+          value={playlistId}
+          onChange={(e) => setPlaylistId(e.target.value)}
+        />
+        <div className="form-buttons">
+          <label>
+            <input
+              type="radio"
+              value={5}
+              checked={numTracks === 5}
+              onChange={() => setNumTracks(5)}
+            />
+            5
+          </label>
+          <label>
+            <input
+              type="radio"
+              value={10}
+              checked={numTracks === 10}
+              onChange={() => setNumTracks(10)}
+            />
+            10
+          </label>
+          <label>
+            <input
+              type="radio"
+              value={15}
+              checked={numTracks === 15}
+              onChange={() => setNumTracks(15)}
+            />
+            15
+          </label>
+        </div>
+        <button type="submit" className='form-button'>Randomize</button>
+      </form>
+    </div>
   );
 }
 
--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
-const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
 
-export async function redirectToAuthCodeFlow(clientId) {
+export async function redirectToAuthCodeFlow() {
     const verifier = generateCodeVerifier(128);
     const challenge = await generateCodeChallenge(verifier);
 
@@ -36,9 +36,8 @@ async function generateCodeChallenge(codeVerifier) {
         .replace(/=+$/, '');
 }
 
-export async function getAccessToken(clientId, code) {
+export async function getAccessToken(code) {
     const verifier = localStorage.getItem("verifier");
-
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
@@ -52,7 +51,101 @@ export async function getAccessToken(clientId, code) {
         body: params
     });
 
+    const { access_token, refresh_token } = await result.json();
+
+    console.log(access_token);
+    
+    return {access_token, refresh_token};
+}
+
+export async function refreshAccessToken() {
+    const refreshToken = localStorage.getItem('refreshToken');
+
+    if (!refreshToken) {
+        throw new Error('No refresh token available');
+    }
+
+    const params = new URLSearchParams();
+    params.append("client_id", clientId);
+    params.append("grant_type", "refresh_token");
+    params.append("refresh_token", refreshToken);
+
+    const result = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: params
+    });
+
     const { access_token } = await result.json();
+    localStorage.setItem('accessToken', access_token); // Save the new access token
     return access_token;
 }
 
+export async function fetchWebApi(endpoint, method = 'GET', body, token) {
+    console.log(`Fetching: https://api.spotify.com/${endpoint}`);
+    const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        },
+        method,
+        body: body ? JSON.stringify(body) : null
+    });
+
+    if (!res.ok) {
+        if (res.status === 401) {
+            // Token expired, refresh it
+            const newToken = await refreshAccessToken();
+            console.log("access token refreshed!");
+            return fetchWebApi(endpoint, method, body, newToken); // Retry with new token
+        }
+        console.error('Error fetching data:', res.status, res.statusText);
+        return null;
+    }
+
+    try {
+        const text = await res.text();
+        return text ? JSON.parse(text) : {}; // Handle empty responses
+    } catch (error) {
+        console.error('Error parsing JSON:', error);
+        return null;
+    }
+}
+
+
+export async function getItemsFromPlaylist(playlistId, token) {
+    console.log(`Getting items from playlist: ${playlistId}`);
+
+    let allItems = [];
+    let offset = 0;
+    let limit = 100;
+    let hasMoreTracks = true;
+
+    while (hasMoreTracks) {
+        const data = await fetchWebApi(`v1/playlists/${playlistId}/tracks?offset=${offset}&limit=${limit}`, 'GET', null, token);
+        if (!data) break;
+        allItems = allItems.concat(data.items);
+        offset += limit;
+        hasMoreTracks = data.items.length === limit;
+    }
+
+    return allItems;
+}
+
+export function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+export async function addToQueue(trackUri, token) {
+    const endpoint = `v1/me/player/queue?uri=${trackUri}`;
+    const result = await fetchWebApi(endpoint, 'POST', null, token);
+    if (result) {
+        console.log(`Track added to queue: ${trackUri}`);
+    } else {
+        console.error('Failed to add track to queue');
+    }
+}
